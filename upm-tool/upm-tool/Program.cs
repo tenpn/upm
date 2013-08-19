1673828e90bde16a1e397246612fc695fc091d60
Andrew Fray <andrew@spryfox.com>
2013-08-19 21:38:07 +0100

    remove unused code

--- upm-tool/upm-tool/Program.cs
+++ upm-tool/upm-tool/Program.cs
@@ -7,9 +7,6 @@ namespace upmtool
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
-			foreach (var packageName in GithubPackagesStore.GetPackages ()) {
-				Console.WriteLine (packageName);
-			}
 		}
 	}
 }
